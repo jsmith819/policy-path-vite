f3f31fcb9bfa4a27593cebf0b0d613350b267673
Nick McDonald <nickmcdonald@macbookpro.lan>
2025-09-03 18:01:55 +1000

    the environment commit

--- src/main.ts
+++ src/main.ts
@@ -49,7 +49,7 @@ const policiesData: Record<string, string[]> = {
   ],
 
   'Care and services': ['commitment statement','3.1 Assessment & planning', '3.2 Delivery of services'],
-  'The environment': ['4.1a Services in home', '4.1b Services outside home'],
+  'The environment': ['commitment statement','4.1a Services in home', '4.1b Services outside home'],
   'Clinical care': ['5.1 Clinical governance', '5.2 Infection control'],
   'Food and nutrition': ['6.1 Partnering on food', '6.2 Nutrition assessment'],
   'Residential community': ['7.1 Daily living', '7.2 Transitions']
